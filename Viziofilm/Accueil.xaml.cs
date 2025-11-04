c42327fe4c4a9bc6a636ef0e5473802724ed13d1
Valerie <stpierre.v2016@gmail.com>
2025-11-04 16:18:03 -0500

    Ajoutez des fichiers projet.

---
+++ Viziofilm/Accueil.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Viziofilm
+{
+	/// <summary>
+	/// Logique d'interaction pour Accueil.xaml
+	/// </summary>
+	public partial class Accueil : Window
+	{
+		public Accueil()
+		{
+			InitializeComponent();
+		}
+
+		private void BtnInvite_Click(object sender, RoutedEventArgs e)
+		{
+			CatalogueInvite nouvelleFenetre = new CatalogueInvite();
+
+			nouvelleFenetre.Show();
+
+			this.Close();
+
+		}
+
+		private void BtnInscription_Click(object sender, RoutedEventArgs e)
+		{
+			Inscription nouvelleFenetre = new Inscription();
+
+			nouvelleFenetre.Show();
+
+			this.Close();
+		}
+
+		private void BtnSAuthentifier_Click(object sender, RoutedEventArgs e)
+		{
+			CatalogueMembre nouvelleFenetre = new CatalogueMembre();
+
+			nouvelleFenetre.Show();
+
+			this.Close();
+		}
+	}
+}
---
+++ Viziofilm/Film.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Viziofilm
+{
+    /// <summary>
+    /// Logique d'interaction pour Film.xaml
+    /// </summary>
+    public partial class Film : Window
+    {
+        public Film()
+        {
+            InitializeComponent();
+        }
+    }
+}
